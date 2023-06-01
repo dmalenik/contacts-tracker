74acebe96e8b28462ca0a668c16d5eb1919ee3be
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-01 16:23:19 +0300

    feat: set main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World!</div>
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
