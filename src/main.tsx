b0177f9bd8194739a12f1c5e9048d39f8286c9e5
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-01 16:24:28 +0300

    feat: create and implement Root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/roots';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Root />,
   },
 ]);
 
