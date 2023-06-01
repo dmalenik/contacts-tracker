1190d93203e6b41cb38c67e57388b045a95dac17
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-01 16:24:55 +0300

    feat: create and implement ErrorPage component

---
+++ src/error-page.tsx
@@ -0,0 +1,22 @@
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+function ErrorPage(): JSX.Element {
+  const error: unknown = useRouteError();
+  console.error(error);
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div id='error-page'>
+        <h1>Ooops!</h1>
+        <p>Sorry, an unexpected error has occured.</p>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+      </div>
+    );
+  } else {
+    return <div>Oops!</div>;
+  }
+}
+
+export default ErrorPage;
