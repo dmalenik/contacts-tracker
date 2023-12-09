ff950bedee61ef65e545daee0b6c7734b69c950f
Dmytro Malienik <dmalenik@gmail.com>
2023-12-09 14:46:09 +0300

    fix(contacts): disable fast refresh for React components

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Form, useLoaderData, useFetcher } from "react-router-dom";
 import { getContact, updateContact } from "../contacts";
 
