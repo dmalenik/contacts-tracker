9e396c6ccf9bf6c142ef035577c9ff52d3dfc9fb
Dmytro Malienik <dmalenik@gmail.com>
2023-12-09 17:33:57 +0300

    fix(destroy action): add types to params

--- src/routes/destroy.tsx
+++ src/routes/destroy.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
-async function action({ params }) {
+async function action({ params }: { params: { contactId: string } }) {
   throw new Error("oh dang!");
 
   await deleteContact(params.contactId);
